ee3db05063c126de7e4565e1dfec5940c5003a81
Karan Visani <karanvisani719@gmail.com>
2026-07-12 09:52:05 +0530

    Setup MongoDB configuration

--- server/server.js
+++ server/server.js
@@ -1,13 +1,31 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-const PORT = 5000;
+app.use(cors());
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.send("TransitOps Backend Running");
+  res.json({
+    success: true,
+    message: "TransitOps Backend Running"
+  });
 });
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
